ceb73fd29ea2c65e8cdd930947856b140a91b64c
Barry G. Becker <barrybecker4@yahoo.com>
2015-12-23 19:26:38 -0800

    Make the Cave grid size match the initial area of the window that displays it.

--- src/cave/CaveGame.java
+++ src/cave/CaveGame.java
@@ -4,20 +4,20 @@ import javax.swing.*;
 
 public class CaveGame {
 
+	private static final int RESOLUTION = 64;
+
 	public CaveGame() {
-		CaveMap cave = new CaveMap();
+		CaveMap cave = new CaveMap(RESOLUTION, RESOLUTION);
 		JFrame frame = new JFrame("Java Game");
 		DrawPanel panel = new DrawPanel(cave);
 		frame.getContentPane().add(panel);
-		frame.setSize(RESOLUTION, RESOLUTION);
+		frame.setSize(RESOLUTION * 10, RESOLUTION * 10);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		cave.printMap();
 		cave.nextPhase();
 		cave.printMap();
 	}
-
-	private static final int RESOLUTION = 512;
 
 	public static void main(String[] args) {
 		new CaveGame();
--- src/cave/DrawPanel.java
+++ src/cave/DrawPanel.java
@@ -5,23 +5,25 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 import javax.swing.*;
 
-public class DrawPanel extends JPanel implements ActionListener{
+public class DrawPanel extends JPanel implements ActionListener {
+
 	private CaveMap cave;
+
 	public DrawPanel(CaveMap cave){
 		this.cave = cave;
 	}
+
 	public void paint(Graphics g){
 		super.paint(g);
-		for(int y=0;y<32;y++){
-			for(int x=0;x<32;x++){
-				if(cave.getMap(x, y)) g.fillRect(x*10, y*10, 10, 10);
+		for(int y=0; y<cave.getHeight(); y++){
+			for(int x=0; x<cave.getWidth(); x++){
+				if(cave.getMap(x, y)) g.fillRect(x * 10, y * 10, 10, 10);
 			}
 		}
 	}
 
-	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
